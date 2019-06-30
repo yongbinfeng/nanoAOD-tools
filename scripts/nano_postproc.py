dcc15e3b35f1901bc0fa03dab9ebc33678c519e2
Yongbin Feng <ybfeng94@gmail.com>
2019-06-30 22:24:14 +0200

    first commit to submit condor jobs

--- scripts/nano_postproc.py
+++ scripts/nano_postproc.py
@@ -4,8 +4,65 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+import re, time
 
-if __name__ == "__main__":
+def prepare_condor_jobs(arglist):
+
+    inputdir = arglist[2]
+
+    for ifile in os.listdir(inputdir):
+        res = re.match("myNanoProdMc2016_NANO_10(\d+).root", ifile)
+        if res is None:
+           continue
+        fname = inputdir + "/" + ifile
+        short = res.group(1)
+
+        # prepare the log directory
+        logdir = os.environ['PWD']+'/'+ "logs"
+        logdir = logdir.rstrip("/")
+        if not os.path.exists(logdir):
+            os.system("mkdir -p "+logdir)
+
+        # prepare the sh script
+        argstring = "python"
+        for iarg in range(len(arglist)):
+            arg = arglist[iarg]
+            if iarg == 2:
+               arg += ifile
+            if "--condor" in arg:
+               continue
+            if arglist[iarg-1] ==  "-c" or arglist[iarg-1]== "--cut":
+               arg = "\"" + arg + "\""
+            argstring += " " + arg
+        print argstring
+        condor_exec="{logdir}/{data}.sh".format(logdir=logdir, data=short)
+        os.system("echo '#!/bin/bash' > {condor_exec} ; echo 'source /afs/cern.ch/work/y/yofeng/public/PFCand/CMSSW_10_2_10/src/PhysicsTools/NanoAODTools/scripts/setup_env.sh' >> {condor_exec};echo '{cmd}' >> {condor_exec} ; chmod u+x {condor_exec}".format(cmd=argstring, data=short, condor_exec=condor_exec))
+       
+        # write the submission script
+        job_desc = """Universe = vanilla
+Executable = {condor_exec}
+use_x509userproxy = $ENV(X509_USER_PROXY)
+Log        = {pid}.log
+Output     = {pid}.out
+Error      = {pid}.error
+getenv      = True
+environment = "LS_SUBCWD={here}"
++JobFlavour = "espresso"
+queue 1\n
+""".format(
+           here=os.environ['PWD'],
+           condor_exec = condor_exec,
+           pid= logdir +"/" + short,
+           )
+        jobdesc_file = logdir+"/"+ short+'.condor'
+        print jobdesc_file
+        with open(jobdesc_file,'w') as outputfile:
+            outputfile.write(job_desc)
+            outputfile.close()
+        os.system( "condor_submit " + jobdesc_file )
+        time.sleep(2)
+
+def main():
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] outputDir inputFiles")
     parser.add_option("-s", "--postfix",dest="postfix", type="string", default=None, help="Postfix which will be appended to the file name (default: _Friend for friends, _Skim for skims)")
@@ -24,8 +81,17 @@ if __name__ == "__main__":
     parser.add_option("--justcount",   dest="justcount", default=False, action="store_true",  help="Just report the number of selected events") 
     parser.add_option("-I", "--import", dest="imports",  type="string", default=[], action="append", nargs=2, help="Import modules (python package, comma-separated list of ");
     parser.add_option("-z", "--compression",  dest="compression", type="string", default=("LZMA:9"), help="Compression: none, or (algo):(level) ")
+    parser.add_option("--condor",   dest="condor", action="store_true", default=False, help="To run the jobs on condor. One file per job");
 
+    arglist = sys.argv
+    print arglist
     (options, args) = parser.parse_args()
+
+    if options.condor:
+        print "prepare the files and submit the job"
+        prepare_condor_jobs( arglist )
+
+        return 1
 
     if options.friend:
         if options.cut or options.json: raise RuntimeError("Can't apply JSON or cut selection when producing friends")
@@ -55,6 +121,8 @@ if __name__ == "__main__":
     if options.branchsel!=None:
         options.branchsel_in = options.branchsel
         options.branchsel_out = options.branchsel
+
+    # run the PostProcessor 
     p=PostProcessor(outdir,args,
             cut = options.cut,
             branchsel = options.branchsel_in,
@@ -71,4 +139,7 @@ if __name__ == "__main__":
             firstEntry = options.firstEntry,
             outputbranchsel = options.branchsel_out)
     p.run()
+
+if __name__ == "__main__":
+   main()
 
